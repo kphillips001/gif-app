8d8e6ca6b64c6c9802932e0e89bcadf87b0c1639
Kevin <knphillips01@gmail.com>
2020-09-25 14:10:42 -0500

    first commit

---
+++ src/components/Random_V1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const API_KEY = process.env.REACT_APP_API_KEY
+
+const Random = () => {
+  return (
+    <>
+      <h1>Random</h1>
+    </>
+  );
+}
+
+export default Random; 
