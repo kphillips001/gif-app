33dc85ea587049142148add757787da66786fd5d
Kevin <knphillips01@gmail.com>
2020-09-25 16:06:34 -0500

    done with project

---
+++ src/components/Randon_V2.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import useGif from '../useGif';
+
+const Tag = () => {
+  const {gif, fetchGif} = useGif(tag);
+  
+  return (
+    <div className='container'>
+      <h1>Random Gif</h1>
+      <img width='500' src={gif} alt='Random Gif' />
+      <button onClick={fetchGif}>ClICK FOR NEW</button>
+    </div>
+  );
+}
+
+
+export default Tag; 
