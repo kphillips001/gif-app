fb55f9bf0321b981e76ab5d5b42b2b24e3f5de51
Kevin <knphillips01@gmail.com>
2020-09-25 14:47:12 -0500

    api working

--- src/components/Random_V1.js
+++ src/components/Random_V1.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const API_KEY = process.env.REACT_APP_API_KEY
+const API_KEY = process.env.REACT_APP_API_KEY;
 
+//aysnc await
 const Random = () => {
+  const [gif, setGif] = useState('');
+
+  const fetchGif = async () => {
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+
+    const { data } = await axios.get(url);
+
+    const ImageSrc = data.data.images.downsized_large.url;
+
+    setGif(ImageSrc);
+  }
+
+  useEffect(() => {
+    fetchGif()
+  }, [])
+
+  const handleClick = () => {
+    fetchGif()
+  }
+
   return (
-    <>
-      <h1>Random</h1>
-    </>
+    <div className='container'>
+      <h1>Random Gif</h1>
+      <img width='500' src={gif} alt='Random Gif' />
+      <button onClick={handleClick}>ClICK FOR NEW</button>
+    </div>
   );
 }
 
